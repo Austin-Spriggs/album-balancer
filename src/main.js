4750f1b61972910f32cd246d1944a078bd93b303
Austin Spriggs <austinspriggs@madisoncollege.edu>
2025-08-29 20:34:07 -0500

    added pwa functionality back

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';;
-// import { registerSW } from 'virtual:pwa-register';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from './App.vue';
 import router from './router/router';
 
 const app = createApp(App);
 
-// const updateSW = registerSW({
-//     onNeedRefresh() {
-//         if (confirm('New version available. Reload?')) {
-//             updateSW(true);
-//         }
-//     },
-//     onOfflineReady() {
-//         console.log('App ready to work offline');
-//     }
-// });
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm('New version available. Reload?')) {
+            updateSW(true);
+        }
+    },
+    onOfflineReady() {
+        console.log('App ready to work offline');
+    }
+});
 
 app.use(createPinia());
 app.use(router);
