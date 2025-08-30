2cd2bdd65884fd8a39efb194b77829ae7f8d0306
Austin Spriggs <austinspriggs@madisoncollege.edu>
2025-08-29 20:26:12 -0500

    hopefully it works now

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -11,29 +11,29 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		vueDevTools(),
-		VitePWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				"name": "Album Balancer",
-				"short_name": "Balancer",
-				"start_url": "/album-balancer/",
-				"display": "standalone",
-				"background_color": "#fff",
-				"theme_color": "#343434",
-				"icons": [
-					{
-						"src": "images/icon-192.png",
-						"sizes": "192x192",
-						"type": "image/png"
-					},
-					{
-						"src": "images/icon-512.png",
-						"sizes": "512x512",
-						"type": "image/png"
-					}
-				]
-			}
-		})
+		// VitePWA({
+		// 	registerType: 'autoUpdate',
+		// 	manifest: {
+		// 		"name": "Album Balancer",
+		// 		"short_name": "Balancer",
+		// 		"start_url": "/album-balancer/",
+		// 		"display": "standalone",
+		// 		"background_color": "#fff",
+		// 		"theme_color": "#343434",
+		// 		"icons": [
+		// 			{
+		// 				"src": "images/icon-192.png",
+		// 				"sizes": "192x192",
+		// 				"type": "image/png"
+		// 			},
+		// 			{
+		// 				"src": "images/icon-512.png",
+		// 				"sizes": "512x512",
+		// 				"type": "image/png"
+		// 			}
+		// 		]
+		// 	}
+		// })
 	],
 	base: '/album-balancer/',
 	resolve: {
